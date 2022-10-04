5c8660cb375c483c92295d58c138b852f5864d34
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-10-04 17:56:47 +0200

    feat: add cleanup functions

---
+++ src/clean.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 20:42:31 by troberts          #+#    #+#             */
+/*   Updated: 2022/10/04 16:03:18 by troberts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	clean_window_display(t_mlx mlx, int return_code)
+{
+	mlx_destroy_window(mlx.mlx_ptr, mlx.win_ptr);
+	return (clean_display(mlx, return_code));
+}
+
+int	clean_display(t_mlx mlx, int return_code)
+{
+	mlx_destroy_display(mlx.mlx_ptr);
+	free(mlx.mlx_ptr);
+	return (return_code);
+}
