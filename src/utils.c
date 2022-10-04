865c47e54584b38ad643c4bf7819e459bba92f0d
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-10-04 17:57:34 +0200

    feat: better mlx_new_image()

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 01:26:58 by troberts          #+#    #+#             */
-/*   Updated: 2022/09/25 20:47:03 by troberts         ###   ########.fr       */
+/*   Updated: 2022/10/03 21:01:27 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	close_window(t_mlx *mlx)
 	return (RETURN_SUCCESS);
 }
 
-void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	ft_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
 	char	*dst;
 	size_t	offset;
@@ -26,4 +26,16 @@ void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
 	offset = y * data->line_length + x * (data->bits_per_pixel / 8);
 	dst = data->addr + offset;
 	*(unsigned int *)dst = color;
+}
+
+int	ft_mlx_new_image(t_mlx mlx, t_img *img)
+{
+	img->img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_W, WIN_H);
+	if (img->img_ptr == NULL)
+		return (RETURN_FAILURE);
+	img->addr = mlx_get_data_addr(img->img_ptr, &img->bits_per_pixel, \
+										&img->line_length, &img->endian);
+	if (img->addr == NULL)
+		return (RETURN_FAILURE);
+	return (RETURN_SUCCESS);
 }
