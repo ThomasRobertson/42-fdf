75a3264a82fd3f6cb2d577c03abc63ac109c4b0a
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-12-10 17:25:54 +0100

    refactor: margin value are not hardcoded anymore

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 19:55:14 by troberts          #+#    #+#             */
-/*   Updated: 2022/12/06 21:29:27 by troberts         ###   ########.fr       */
+/*   Updated: 2022/12/08 00:20:56 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	first_render(t_map_data *map, t_img img, t_mlx mlx)
 	map->angle = ANGLE;
 	map->proj = DEFAULT_PROJ;
 	map->overwrite_color = true;
+	map->margin.bottom = BOTTOM_MARGIN;
+	map->margin.top = TOP_MARGIN;
+	map->margin.right = RIGHT_MARGIN;
+	map->margin.left = LEFT_MARGIN;
 	find_max_min(map);
 	fill_background(img);
 	draw_3d(*map, img);
