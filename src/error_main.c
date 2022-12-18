4fc5fe2d72b48d6932f1f68c91c661a6375203b2
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-12-18 15:33:36 +0100

    fix: change fd for error message

--- src/error_main.c
+++ src/error_main.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 19:50:56 by troberts          #+#    #+#             */
-/*   Updated: 2022/11/28 20:19:40 by troberts         ###   ########.fr       */
+/*   Updated: 2022/12/18 02:43:57 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 int	error_args(void)
 {
-	ft_printf("Usage: ./fdf [MAP]\n");
+	ft_putendl_fd("Usage: ./fdf [MAP]\n", STDERR_FILENO);
 	return (EXIT_FAILURE);
 }
 
 int	error_init_windows(t_map_data map)
 {
-	ft_printf("Could not initialized window.\n");
+	ft_putendl_fd("Could not initialized window.\n", STDERR_FILENO);
 	clean_map(&map);
 	return (EXIT_FAILURE);
 }
 
 int	error_new_image(t_mlx mlx, t_map_data map)
 {
-	ft_printf("Could not create new image.");
+	ft_putendl_fd("Could not create new image.", STDERR_FILENO);
 	clean_map(&map);
 	return (clean_window_display(mlx, EXIT_FAILURE));
 }
