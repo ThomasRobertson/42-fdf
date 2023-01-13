8c879ee5fa68c9eb9d6b45fb58903aa97a303676
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-01-13 23:36:38 +0100

    refactor: change z to double

--- src/find_min_max.c
+++ src/find_min_max.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 12:37:26 by troberts          #+#    #+#             */
-/*   Updated: 2022/12/05 22:34:18 by troberts         ###   ########.fr       */
+/*   Updated: 2023/01/13 23:34:56 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	find_max_min_z(t_map_data *map)
 {
-	int				x;
-	int				y;
+	double			x;
+	double			y;
 	t_map_point		point;
 
 	y = 0;
@@ -37,8 +37,8 @@ void	find_max_min_z(t_map_data *map)
 
 void	find_max_min_x(t_map_data *map)
 {
-	int				x;
-	int				y;
+	double			x;
+	double			y;
 	t_map_point		point;
 
 	y = 0;
@@ -48,8 +48,7 @@ void	find_max_min_x(t_map_data *map)
 		while (x < map->nbr_line)
 		{
 			point = return_point(x, y, *map);
-			if (map->proj != none)
-				point = convert_point(point, *map);
+			point = convert_point(point, *map);
 			if (point.x > map->x.max)
 				map->x.max = point.x;
 			if (point.x < map->x.min)
@@ -62,8 +61,8 @@ void	find_max_min_x(t_map_data *map)
 
 void	find_max_min_y(t_map_data *map)
 {
-	int				x;
-	int				y;
+	double			x;
+	double			y;
 	t_map_point		point;
 
 	y = 0;
@@ -73,8 +72,7 @@ void	find_max_min_y(t_map_data *map)
 		while (x < map->nbr_line)
 		{
 			point = return_point(x, y, *map);
-			if (map->proj != none)
-				point = convert_point(point, *map);
+			point = convert_point(point, *map);
 			if (point.y > map->y.max)
 				map->y.max = point.y;
 			if (point.y < map->y.min)
@@ -91,8 +89,8 @@ void	find_max_min(t_map_data *map)
 	map->x.max = DBL_MIN;
 	map->y.min = DBL_MAX;
 	map->y.max = DBL_MIN;
-	map->z.min = INT_MAX;
-	map->z.max = INT_MIN;
+	map->z.min = DBL_MAX;
+	map->z.max = DBL_MIN;
 	find_max_min_x(map);
 	find_max_min_y(map);
 	find_max_min_z(map);
