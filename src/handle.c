59829c1415db2ff16152d3934cffcc1775ae9706
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-01-13 14:32:58 +0100

    feat: add reset handle

--- src/handle.c
+++ src/handle.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 20:53:09 by troberts          #+#    #+#             */
-/*   Updated: 2023/01/05 00:41:33 by troberts         ###   ########.fr       */
+/*   Updated: 2023/01/13 14:32:36 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int	handle_keypress(int keysum, t_hook_bundle *hook)
 		handle_zoom_map(keysum, hook);
 	else if (keysum == XK_p || keysum == XK_P)
 		handle_change_projection(hook);
+	else if (keysum == XK_x || keysum == XK_X)
+		handle_reset(hook);
 	return (RETURN_SUCCESS);
 }
 
@@ -87,5 +89,18 @@ int	handle_zoom_map(int keysum, t_hook_bundle *hook)
 		hook->map->margin.right += HORIZONTAL_INCREMENT;
 	}
 	render(*(hook->map), *(hook->img), *(hook->mlx));
+	return (RETURN_SUCCESS);
+}
+
+int	handle_reset(t_hook_bundle *hook)
+{
+	hook->map->rotate_value.x = DEFAULT_ROTATE;
+	hook->map->rotate_value.y = DEFAULT_ROTATE;
+	hook->map->rotate_value.z = DEFAULT_ROTATE;
+	hook->map->margin.bottom = BOTTOM_MARGIN;
+	hook->map->margin.top = TOP_MARGIN;
+	hook->map->margin.left = LEFT_MARGIN;
+	hook->map->margin.right = RIGHT_MARGIN;
+	hook->map->proj = DEFAULT_PROJ;
 	return (RETURN_SUCCESS);
 }
