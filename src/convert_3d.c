bd79e3f2a386ac44d64612e75fa62a73f73b57df
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-01-13 23:37:56 +0100

    refactor: convert doesn't use map anymore

--- src/convert_3d.c
+++ src/convert_3d.c
@@ -6,48 +6,39 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 15:59:32 by troberts          #+#    #+#             */
-/*   Updated: 2023/01/05 00:39:20 by troberts         ###   ########.fr       */
+/*   Updated: 2023/01/10 23:37:25 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_map_point	convert_iso_point(t_map_point point, t_map_data map)
+t_map_point	convert_iso_point(t_map_point point)
 {
-	double	previous_x;
-	double	previous_y;
+	t_map_point	old;
 
-	previous_x = point.x;
-	previous_y = point.y;
-	point.x = (previous_x - previous_y) * cos(convert_to_rad(map.angle));
-	point.y = (previous_x + previous_y) * sin(convert_to_rad(map.angle)) \
-																	- point.z;
+	old = point;
+	point.x = (old.x - old.y) * cos(convert_to_rad(30));
+	point.y = (old.x + old.y) * sin(convert_to_rad(30)) - point.z;
 	return (point);
 }
 
-t_map_point	convert_para_point(t_map_point point, t_map_data map)
+t_map_point	convert_para_point(t_map_point point)
 {
-	double	previous_x;
-	double	previous_y;
+	t_map_point	old;
 
-	(void)map;
-	previous_x = point.x;
-	previous_y = point.y;
-	point.x = previous_x - point.z * cos(convert_to_rad(45));
-	point.y = previous_y - point.z + sin(convert_to_rad(45));
+	old = point;
+	point.x = old.x - point.z * cos(convert_to_rad(45));
+	point.y = old.y - point.z + sin(convert_to_rad(45));
 	return (point);
 }
 
-t_map_point	convert_para2_point(t_map_point point, t_map_data map)
+t_map_point	convert_para2_point(t_map_point point)
 {
-	double	previous_x;
-	double	previous_y;
+	t_map_point	old;
 
-	(void)map;
-	previous_x = point.x;
-	previous_y = point.y;
-	point.x = previous_y + cos(convert_to_rad(60)) * previous_x;
-	point.y = -point.z + sin(convert_to_rad(60)) * previous_x;
+	old = point;
+	point.x = old.y + cos(convert_to_rad(60)) * old.x;
+	point.y = -point.z + sin(convert_to_rad(60)) * old.x;
 	return (point);
 }
 
@@ -56,11 +47,11 @@ t_map_point	convert_point(t_map_point point, t_map_data map)
 	if (map.proj == none)
 		return (point);
 	if (map.proj == iso)
-		return (convert_iso_point(point, map));
+		return (convert_iso_point(point));
 	else if (map.proj == para)
-		return (convert_para_point(point, map));
+		return (convert_para_point(point));
 	else if (map.proj == para2)
-		return (convert_para2_point(point, map));
+		return (convert_para2_point(point));
 	else
-		return (convert_iso_point(point, map));
+		return (convert_iso_point(point));
 }
