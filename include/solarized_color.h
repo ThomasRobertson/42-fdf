64d661e4ac98cf7ba26ce09dc3090b34ae45bde5
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-01-14 17:09:20 +0100

    refactor: use solarized color header

---
+++ include/solarized_color.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solarized_color.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 02:58:41 by troberts          #+#    #+#             */
+/*   Updated: 2023/01/14 03:03:29 by troberts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLARIZED_COLOR_H
+# define SOLARIZED_COLOR_H
+
+# define SOL_BASE03 0XFF002B36
+# define SOL_BASE02 0XFF073642
+# define SOL_BASE01 0XFF586E75
+# define SOL_BASE00 0XFF657B83
+# define SOL_BASE0 0XFF839496
+# define SOL_BASE1 0XFF93A1A1
+# define SOL_BASE2 0XFFEEE8D5
+# define SOL_BASE3 0XFFFDF6E3
+# define SOL_YELLOW 0XFFB58900
+# define SOL_ORANGE 0XFFCB4B16
+# define SOL_RED 0XFFD30102
+# define SOL_MAGENTA 0XFFD33682
+# define SOL_VIOLET 0XFF6C71C4
+# define SOL_BLUE 0XFF268BD2
+# define SOL_CYAN 0XFF2AA198
+# define SOL_GREEN 0XFF859900
+
+#endif
