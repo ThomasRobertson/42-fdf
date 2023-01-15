6f3a7cb45baa69cac26d3455ac6aeadf10b2a6e8
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-01-15 19:12:11 +0100

    feat: add frame sync

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 19:55:14 by troberts          #+#    #+#             */
-/*   Updated: 2023/01/14 14:36:54 by troberts         ###   ########.fr       */
+/*   Updated: 2023/01/15 19:11:56 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	first_render(t_map_data *map, t_img img, t_mlx *mlx)
 	map->rotate_value.y = DEFAULT_ROTATE;
 	map->rotate_value.z = DEFAULT_ROTATE;
 	find_max_min(map);
+	mlx_do_sync(mlx->mlx_ptr);
 	fill_background(img, WIN_W, WIN_H, DEFAULT_COLOR_BG);
 	draw_map(*map, img);
 	mlx_do_key_autorepeaton(mlx->mlx_ptr);
@@ -33,6 +34,7 @@ void	first_render(t_map_data *map, t_img img, t_mlx *mlx)
 
 void	render(t_map_data map, t_img img, t_mlx mlx)
 {
+	mlx_do_sync(mlx.mlx_ptr);
 	fill_background(img, WIN_W, WIN_H, DEFAULT_COLOR_BG);
 	draw_map(map, img);
 	fill_background(img, WIN_W * (double)MENU_W, WIN_H, DEFAULT_COLOR_MENU);
